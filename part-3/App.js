17231f418097090f6c5e5df955ac88dbbe19ded0
heyimmatt <mrmcfarland@gmail.com>
2020-06-25 10:17:26 -0700

    Display person name and age, and some conditional logic what to display

---
+++ part-3/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="people">
+      <Person name="Jimbo" age="45" />
+      <Person name="Stan" age="10" />
+      <Person name="Bartholomew" age="10" />
+    </div>
+  )
+}
