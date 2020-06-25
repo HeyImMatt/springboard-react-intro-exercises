c769b83dcf1f6f8fdc070b1f08b0a296f1b0b147
heyimmatt <mrmcfarland@gmail.com>
2020-06-25 10:39:38 -0700

    complete part 3 of the exercise

--- part-3/App.js
+++ part-3/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div className="people">
-      <Person name="Jimbo" age="45" />
-      <Person name="Stan" age="10" />
-      <Person name="Bartholomew" age="10" />
+      <Person name="Jimbo" age="45" hobbies={['Hunting', 'Beer']}/>
+      <Person name="Stan" age="10" hobbies={['Football', 'Lord of the Rings']}/>
+      <Person name="Bartholomew" age="12" hobbies={['Skateboarding', 'Graffiti']}/>
     </div>
   )
 }
