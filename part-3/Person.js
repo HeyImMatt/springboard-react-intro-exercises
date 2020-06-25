f15aaea92228c1cad0b251126b3cc02a3c635077
heyimmatt <mrmcfarland@gmail.com>
2020-06-25 10:18:13 -0700

    Fix typo showing name label instead of age

--- part-3/Person.js
+++ part-3/Person.js
@@ -3,7 +3,7 @@ function Person(props) {
   <div className="person">
     <p>Learn some information about this person:</p>
     <p>Name: {props.name.length < 8 ? props.name : props.name.substring(0, 6) + '...'}</p>
-    <p>Name: {props.age}</p>
+    <p>Age: {props.age}</p>
     <h3>{props.age >= 18 ? 'Please go vote!' : 'You must be 18 to vote'}</h3>
   </div>
   )
